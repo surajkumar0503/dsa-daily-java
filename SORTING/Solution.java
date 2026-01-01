994d4f9bd8b81a3b9c6ce1f716ffaa8d2ed42937
SURAJ KUMAR <103713574+surajkumar0503@users.noreply.github.com>
2026-01-01 22:57:59 +0530

    Add bubbleSort and selectionSort methods
    Implemented bubble sort and selection sort algorithms.

--- SORTING/Solution.java
+++ SORTING/Solution.java
@@ -12,6 +12,47 @@ public class Solution {
         return true; // never broke, so sorted
     }
 
+    // Q2. Given an array, arr[]. Sort the array using bubble sort algorithm.
+    public void bubbleSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        int swaps = 0;
+        for(int i = 0; i < n-1; i++){ //n-1 pass
+        
+            for(int j = 0; j < n-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                      swaps++;
+                }
+            }
+            if(swaps == 0) break;
+          }
+    }
+
+    // Q3. Given an array arr, use selection sort to sort arr[] in increasing order.
+    void selectionSort(int[] arr) {
+        
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++){// n-1 pass
+            int min = Integer.MAX_VALUE;
+            int minIdx = -1;
+            for(int j = i; j < n; j++){
+                if(arr[j] < min){
+                    min = arr[j];
+                    minIdx = j; //indexes exchange
+                }
+            }
+            int temp= arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+            
+        }
+        int minIdx = -1;
+    }
+    
     public static void main(String[] args) {
         int[] arr = {2, 3, 9, 7};
         System.out.println(isSorted(arr));
